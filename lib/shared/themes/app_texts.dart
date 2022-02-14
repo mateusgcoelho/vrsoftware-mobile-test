52a3b662db1fc1ababefb2a4786f5812b6741159
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2022-02-14 17:41:44 -0300

    Adding GoogleFonts package!

--- lib/shared/themes/app_texts.dart
+++ lib/shared/themes/app_texts.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile/shared/themes/app_colors.dart';
 
 class AppTexts {
-  static final appBar = TextStyle(
+  static final appBar = GoogleFonts.roboto(
     color: AppColors.black,
     fontSize: 24,
+    fontWeight: FontWeight.bold,
   );
 }
