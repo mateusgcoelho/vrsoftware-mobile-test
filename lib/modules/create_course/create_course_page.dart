e7666c54bd818af34e97e334e4771c2d1aefc69a
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2022-02-15 18:33:03 -0300

    Refatoring create_course_page!

--- lib/modules/create_course/create_course_page.dart
+++ lib/modules/create_course/create_course_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/shared/themes/app_colors.dart';
+import 'package:mobile/shared/themes/app_texts.dart';
 import 'package:mobile/shared/widgets/button_widget.dart';
 import 'package:mobile/shared/widgets/input_widget.dart';
 
@@ -8,34 +10,66 @@ class CreateCoursePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Registrar Curso"),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 32,
-            ),
-            child: Form(
-              child: Column(
-                children: [
-                  InputTextWidget(labelText: "Nome", onChanged: (value) {}),
-                  InputTextWidget(
-                    labelText: "Descrição",
-                    minLines: 4,
-                    maxLines: 4,
-                    onChanged: (value) {},
-                  ),
-                  ButtonWidget(
-                    text: "Registrar",
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 136,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.chevron_left,
+                      ),
+                      color: AppColors.primary,
+                      iconSize: 26,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Text(
+                      "Novo curso",
+                      style: AppTexts.courseInfoAppBar,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      width: 48,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 32,
+              ),
+              child: Form(
+                child: Column(
+                  children: [
+                    InputTextWidget(labelText: "Nome", onChanged: (value) {}),
+                    InputTextWidget(
+                      labelText: "Descrição",
+                      minLines: 8,
+                      maxLines: 8,
+                      onChanged: (value) {},
+                    ),
+                    ButtonWidget(
+                      text: "Cadastrar",
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
